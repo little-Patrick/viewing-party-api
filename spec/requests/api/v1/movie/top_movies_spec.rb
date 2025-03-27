65aad44b9d77be1841840a54f767371742b2738c
little-Patrick <Little.patrick@outlook.com>
2025-03-27 16:40:58 -0600

    Test/Fix:

--- spec/requests/api/v1/movie/top_movies_spec.rb
+++ spec/requests/api/v1/movie/top_movies_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "Api::V1::Movie::TopMovies", type: :request do
        
       expect(response).to have_http_status(:success)
       movies = JSON.parse(response.body, symbolize_names: true)
-      expect(movies[:data].count).to be(20)
+      expect(movies[:data].count).to be <=20
 
       movies[:data].each do |movie|
         expect(movie).to have_key(:id)
