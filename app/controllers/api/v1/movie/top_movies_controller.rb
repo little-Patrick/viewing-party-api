4451fdcae86e3845d369930c59d3e61e90f2be16
little-Patrick <Little.patrick@outlook.com>
2025-03-26 20:52:02 -0600

    Add/Fix: index to show, added path for finding top movies

--- app/controllers/api/v1/movie/top_movies_controller.rb
+++ app/controllers/api/v1/movie/top_movies_controller.rb
@@ -1,4 +1,15 @@
 class Api::V1::Movie::TopMoviesController < ApplicationController
-  def index
+  def show
+    api_key = Rails.application.credentials.tmdb[:key]
+    
+    conn = Faraday.new(url: "https://api.themoviedb.org") 
+    # do |faraday|
+    #   faraday.headers["Authorization"] = "Bearer #{api_key}"
+    # end
+
+    response = conn.get("/3/movie/top_rated", { api_key: api_key })
+    top_movies = JSON.parse(response.body, symbolize_names: true)
+    
+    render json: MovieSerializer.format_movies(top_movies)
   end
 end
