fad77e4e0a37b4e4060a68539358b726c4c6af3c
little-Patrick <Little.patrick@outlook.com>
2025-03-27 21:09:47 -0600

    Add: validation to ViewingPary

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,4 +1,7 @@
 class ViewingParty < ApplicationRecord
   has_many :invitations
   has_many :users, through: :invitations
+
+  validates :name, :start_time, :end_time, :movie_id, :movie_title, :host_id, presence: true
+
 end
