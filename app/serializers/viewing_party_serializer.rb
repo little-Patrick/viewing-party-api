684128dbffc903c2ff45022f91104b8214984369
little-Patrick <Little.patrick@outlook.com>
2025-03-31 12:51:54 -0600

    Fix: iterating over response

--- app/serializers/viewing_party_serializer.rb
+++ app/serializers/viewing_party_serializer.rb
@@ -1,29 +1,27 @@
-class VeiwingPartySerializer
-  def self.format_partys(partys)
-    { data:
-      partys[:results].map do |party|
-          {
-            id: party[:id].to_s,
-            type: "viewing_party",
-            attributes: {
-              name: party[:name],
-              start_time: party[:startTime],
-              end_time: party[:endTime],
-              movie_id: party[movieId],
-              movie_title: party[movieTitle],
-              invitees: format_invitees(Invitation.invitee_lookup(party[:invitees]))
-            }
-          }
-      end
+class ViewingPartySerializer
+  def self.format_parties(party)
+    {
+      data: {
+        id: party.id.to_s,
+        type: "viewing_party",
+        attributes: {
+          name: party.name,
+          start_time: party.start_time,
+          end_time: party.end_time,
+          movie_id: party.movie_id,
+          movie_title: party.movie_title,
+          invitees: format_invitees(party.users)
+        }
+      }
     }
   end
 
-  def format_invitees(invitees)
+  def self.format_invitees(invitees)
     invitees.map do |invitee|
       {
-        id: invitee[:id],
-        name: invitee[:name],
-        username: invitee[:username]
+        id: invitee.id,
+        name: invitee.name,
+        username: invitee.username
       }
     end
   end
