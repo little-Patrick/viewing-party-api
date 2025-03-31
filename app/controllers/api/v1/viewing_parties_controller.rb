5da70f94c39703f732e672398edf65b34dfe5833
little-Patrick <Little.patrick@outlook.com>
2025-03-31 10:19:05 -0600

    Add: routing for creating a viewing party

--- app/controllers/api/v1/viewing_parties_controller.rb
+++ app/controllers/api/v1/viewing_parties_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::ViewingPartiesController < ApplicationController
   def create
+    party = Item.create!(viewing_party_params)
+    render json: ViewingPartySerializer.new(party), status: :created
+  end
+
+  private
+
+  def viewing_party_params
+    params.permit(:name, :start_time, :end_time, :movie_id, :movie_title, :host_id)
   end
 end
