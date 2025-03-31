911973c07fef295c20d8916c79833ddcba363c2c
little-Patrick <Little.patrick@outlook.com>
2025-03-31 17:51:26 -0600

    Add: Serializer for details of a movie

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -13,4 +13,42 @@ class MovieSerializer
       end.first(20)
     }
   end
+
+  def self.format_movie_details(movie)
+    {
+      data: {
+        id: movie.id.to_s,
+        type: "movie",
+        attributes: {
+          title: movie.title,
+          release_year: movie.release_date.split("-").first.to_i, # Extract year from release_date
+          vote_average: movie.vote_average,
+          runtime: format_runtime(movie.runtime),
+          genres: movie.genres.map { |genre| genre[:name] },
+          summary: movie.summary,
+          cast: movie.cast.first(10).map do |member|
+            {
+              character: member[:character],
+              actor: member[:name]
+            }
+          end,
+          total_reviews: movie.reviews.size,
+          reviews: movie.reviews.first(5).map do |review|
+            {
+              author: review[:author],
+              review: review[:content]
+            }
+          end
+        }
+      }
+    }
+  end
+
+  private
+
+  def self.format_runtime(minutes)
+    hours = minutes / 60
+    mins = minutes % 60
+    "#{hours} hours, #{mins} minutes"
+  end
 end
