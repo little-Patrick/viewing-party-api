3ea6703b1dc76b9d34cf132ee4d2bda438a182be
little-Patrick <Little.patrick@outlook.com>
2025-03-31 10:18:22 -0600

    Add: method for finding invitees in a veiwing party

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,8 @@
 class Invitation < ApplicationRecord
   belongs_to :viewing_party
   belongs_to :user
+
+  def self.invitee_lookup(invitees)
+    User.where(id: invitees).pluck(:id, :name, :username)
+  end
 end
