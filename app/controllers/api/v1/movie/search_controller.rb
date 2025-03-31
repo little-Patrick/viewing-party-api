6849ba1a725d9071f67c8d9f022111ebc71af158
little-Patrick <Little.patrick@outlook.com>
2025-03-31 17:49:49 -0600

    Chore:

--- app/controllers/api/v1/movie/search_controller.rb
+++ app/controllers/api/v1/movie/search_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::Movie::SearchController < ApplicationController
   def index
     searched_movies = MovieGateway.search_movies(params[:movie])
-    
     render json: MovieSerializer.format_movies(searched_movies)
   end
 end
