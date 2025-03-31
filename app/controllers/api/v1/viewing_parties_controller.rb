0ae8e253f8608514e0e514077dab4e0020c9e5cc
little-Patrick <Little.patrick@outlook.com>
2025-03-31 12:51:07 -0600

    Fix: route for creating a viewing party

--- app/controllers/api/v1/viewing_parties_controller.rb
+++ app/controllers/api/v1/viewing_parties_controller.rb
@@ -1,11 +1,19 @@
 class Api::V1::ViewingPartiesController < ApplicationController
   def create
-    party = Item.create!(viewing_party_params)
-    render json: ViewingPartySerializer.new(party), status: :created
+    invitees = params[:invitees] || []
+    
+    viewing_party_attributes = params.permit(:name, :start_time, :end_time, :movie_id, :movie_title, :host_id)
+    party = ViewingParty.create!(viewing_party_attributes)
+    
+    invitees.each do |user_id|
+      party.invitations.create(user_id: user_id)
+    end
+    
+    render json: ViewingPartySerializer.format_parties(party), status: :created
   end
-
+  
   private
-
+  
   def viewing_party_params
     params.permit(:name, :start_time, :end_time, :movie_id, :movie_title, :host_id)
   end
