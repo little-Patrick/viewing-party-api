e59a402ff3108e7514a1a5964916f7a4c35c3672
little-Patrick <Little.patrick@outlook.com>
2025-03-31 18:19:02 -0600

    Add: show route to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+  def index
+    render json: UserSerializer.format_user_list(User.all)
+  end
+
+  def show
+    user = User.find_by(id: params[:id])
+    if user
+      render json: UserSerializer.format_user_profile(user)
+    else
+      render json: { message: "Invalid User ID", status: 404 }, status: :not_found
+    end
+  end
+
   def create
     user = User.new(user_params)
     if user.save
@@ -6,10 +19,6 @@ class Api::V1::UsersController < ApplicationController
     else
       render json: ErrorSerializer.format_error(ErrorMessage.new(user.errors.full_messages.to_sentence, 400)), status: :bad_request
     end
-  end
-
-  def index
-    render json: UserSerializer.format_user_list(User.all)
   end
 
   private
